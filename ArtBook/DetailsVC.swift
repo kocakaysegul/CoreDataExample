be96b9f61bb4debd2eabbc94d4b70dc8c30d31d7
kocakaysegul <aysegulkocak112@gmail.com>
2023-02-24 16:23:41 +0300

    design completed

---
+++ ArtBook/DetailsVC.swift
@@ -0,0 +1,37 @@
+//
+//  DetailsVC.swift
+//  ArtBook
+//
+//  Created by Ayşegül Koçak on 24.02.2023.
+//
+
+import UIKit
+
+class DetailsVC: UIViewController {
+
+    @IBOutlet weak var imageView: UIImageView!
+    @IBOutlet weak var nameText: UITextField!
+    @IBOutlet weak var artistText: UITextField!
+    @IBOutlet weak var yearText: UITextField!
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        let gestureRecognizer = UITapGestureRecognizer(target: self, action: #selector(hideKeyboard))
+        view.addGestureRecognizer(gestureRecognizer)
+
+        
+    }
+    
+    @objc func hideKeyboard() {
+        view.endEditing(true)
+    }
+    
+    @IBAction func saveButtonClicked(_ sender: Any) {
+        print("test")
+    }
+    
+ 
+
+}
--- ArtBook/ViewController.swift
+++ ArtBook/ViewController.swift
@@ -8,10 +8,19 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
+    @IBOutlet weak var tableView: UITableView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        
+        navigationController?.navigationBar.topItem?.rightBarButtonItem = UIBarButtonItem(barButtonSystemItem: UIBarButtonItem.SystemItem.add, target: self, action: #selector(addButtonClicked))
+        
+    }
+    
+    @objc func addButtonClicked(){
+        performSegue(withIdentifier: "toDetailsVC", sender: nil)
     }
 
 
