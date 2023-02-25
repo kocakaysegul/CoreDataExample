ae65082501a85aeab4815a86fbd91ffc8a0a4c60
kocakaysegul <aysegulkocak112@gmail.com>
2023-02-25 14:38:29 +0300

    deleting adjusted

--- ArtBook/ViewController.swift
+++ ArtBook/ViewController.swift
@@ -92,6 +92,46 @@ class ViewController: UIViewController, UITableViewDelegate, UITableViewDataSour
         selectedPaintingId = idArray[indexPath.row]
         performSegue(withIdentifier: "toDetailsVC", sender: nil)
     }
+    
+    func tableView(_ tableView: UITableView, commit editingStyle: UITableViewCell.EditingStyle, forRowAt indexPath: IndexPath) {
+        if editingStyle == .delete {
+            let appDelegate = UIApplication.shared.delegate as! AppDelegate
+            let context = appDelegate.persistentContainer.viewContext
+            let fetchRequest = NSFetchRequest<NSFetchRequestResult>(entityName: "Paintings")
+            let idString = idArray[indexPath.row].uuidString
+            fetchRequest.predicate = NSPredicate(format: "id = %@ ", idString)
+            fetchRequest.returnsObjectsAsFaults = false
+            
+            do {
+                let results = try context.fetch(fetchRequest)
+                
+                if results.count > 0 {
+                    for result in results as! [NSManagedObject] {
+                        if let id = result.value(forKey: "id") as? UUID {
+                            if id == idArray[indexPath.row] {
+                                context.delete(result)
+                                nameArray.remove(at: indexPath.row)
+                                idArray.remove(at: indexPath.row)
+                                self.tableView.reloadData()
+                                do {
+                                    try context.save()
+                                } catch {
+                                    print("error")
+                                }
+                                
+                                break
+                            }
+                        }
+                            
+                    }
+                }
+                
+            } catch {
+                print("error")
+            }
+            
+        }
+    }
 
 
 }
